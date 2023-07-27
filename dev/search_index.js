35cd0547d89cc637799c542e80b695878423bae5
Documenter.jl <documenter@juliadocs.github.io>
2023-07-27 19:38:43 +0000

    build based on 50f5e89

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RecursiveFileList","category":"page"},{"location":"#RecursiveFileList","page":"Home","title":"RecursiveFileList","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RecursiveFileList.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RecursiveFileList]","category":"page"}]
+}
